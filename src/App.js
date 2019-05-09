4305bdc10d2182b2076b5ff36f4f58477f9fb18c
geneblandjr@gmail.com <geneblandjr@gmail.com>
2019-05-09 15:34:35 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
         <p className="App-intro">
-          I was changed  Really?.
+          Anything else adding feature branch!
         </p>
         <a
           className="App-link"
